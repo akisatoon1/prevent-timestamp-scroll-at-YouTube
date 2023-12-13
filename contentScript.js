5fdb654c9f421416870590d2164d15fe3f63ab82
akisatoon <akisatoon@gmail.com>
2023-12-14 00:06:26 +0900

    add the first comments

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,32 @@
 "use strict";
 
+// クリックした時
 document.addEventListener("click", (event) => {
+
+    // 動画再生ページかどうか
     if (location.pathname == "/watch") {
+
         const videoID = location.search.slice(3);
+
         const re = RegExp(`t=[0-9]+s`);
+
+        // クリックしたリンクがタイムスタンプかどうか
         if (event.target.tagName == "A" &&
+
             event.target.classList.value == "yt-simple-endpoint style-scope yt-formatted-string" &&
+
             re.test(event.target.getAttribute("href"))
         ) {
             const time = event.target.getAttribute("href").slice(-3, -1);
+
             const videoEle = document.querySelector("video");
+
             videoEle.currentTime = time;
+
             event.preventDefault();
+
             event.stopPropagation();
+
             event.stopImmediatePropagation();
         }
     }
